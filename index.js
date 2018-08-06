c40bcd754f1bb7dc3f5a038d8c3a9e7a43f5c71a
Dee Race <deerace12@gmail.com>
2018-08-06 21:27:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 function BoardMember (name, homeState, training) {
-  this.name = name; 
+  this.name = name;
   this.homeState = homeState;
   this.training = training;
-  this.veto = function(){
+  veto = function(){
     console.log("No, I must disagree");
   }
   this.approve = function(){
